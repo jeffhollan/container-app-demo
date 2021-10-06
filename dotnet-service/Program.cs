a8740900d167a7552c8ceeaf7af2111335d52d3f
Jeff Hollan <jeffhollan@outlook.com>
2021-10-05 22:15:36 -0700

    add swagger

--- dotnet-service/Program.cs
+++ dotnet-service/Program.cs
@@ -1,11 +1,16 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+// My store service
+
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<OrderService, OrderService>();
 builder.Services.AddSingleton<InventoryService, InventoryService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Store API", Description = "Store operations", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -19,4 +24,6 @@ app.MapGet("/inventory",
     (HttpContext context, InventoryService service) => 
     service.GetInventory(context.Request.Query["id"].ToString()));
 
+// http://endpoint/swagger/v1/swagger.json
+app.UseSwagger();
 app.Run();

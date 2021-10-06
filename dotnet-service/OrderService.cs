0d00664c80577b07a346d6ca95e11b81692bd614
Jeff Hollan <jeffhollan@outlook.com>
2021-10-05 22:05:57 -0700

    dotnet calling other service simply

--- dotnet-service/OrderService.cs
+++ dotnet-service/OrderService.cs
@@ -1,5 +1,13 @@
 public class OrderService {
-    public string GetOrder(string orderId) {
-        return $"Order status for {orderId}";
+    private readonly HttpClient httpClient;
+    private readonly string PYTHON_SERVICE = Environment.GetEnvironmentVariable("PYTHON_SERVICE") ?? "localhost:5000";
+
+    public OrderService(IHttpClientFactory httpClientFactory) {
+        this.httpClient = httpClientFactory.CreateClient();
+    }
+
+    public async Task<string> GetOrder(string orderId) {
+        var res = await httpClient.GetAsync($"http://{PYTHON_SERVICE}");
+        return $"Order status for {orderId}. {res.StatusCode}";
     }
 }
--- dotnet-service/Program.cs
+++ dotnet-service/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<OrderService>(new OrderService());
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<OrderService, OrderService>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
 app.MapGet("/order", 
     (HttpContext context, OrderService service) => 
     service.GetOrder(context.Request.Query["id"].ToString()));
